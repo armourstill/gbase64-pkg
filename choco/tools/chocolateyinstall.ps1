c8c73658ca10c714c2037a2fce17fd925cfbac9d
Armourstill <armourstill@hotmail.com>
2025-07-07 15:10:25 +0800

    Add choco package

---
+++ choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipUrl = 'https://github.com/armourstill/gbase64/releases/download/v0.11.0/gbase64-v0.11.0.windows.amd64.zip'
+Install-ChocolateyZipPackage -PackageName 'gbase64' -Url $zipUrl -UnzipLocation $toolsDir
